1e555d2d7282c5f3071a739f3b2ff84d18721484
berberman <1793913507@qq.com>
2019-12-22 16:51:15 +0800

    temp

--- src/main/kotlin/org/mechdancer/nano/Logger.kt
+++ src/main/kotlin/org/mechdancer/nano/Logger.kt
@@ -6,6 +6,9 @@ import org.mechdancer.common.SimpleLogger
 /** 全局日志器 **/
 val globalLogger by lazy { SimpleLogger("nano", "globalLog") }
 
+fun SimpleLogger.info(vararg msg: Any?) =
+    log("[Info]", *msg)
+
 inline fun SimpleLogger.info(block: () -> Any?) =
     log("[Info]", block())
 
--- src/main/kotlin/org/mechdancer/nano/Main.kt
+++ src/main/kotlin/org/mechdancer/nano/Main.kt
@@ -38,34 +38,37 @@ fun test() {
 
 fun main(args: Array<String>) {
 
-//    val data1 = MotorStatePacket(Array(6) { MotorState.Break })
-//    val data2 = MotorStatePacket(Array(6) { MotorState.Stop })
-//    val data3 = MotorStatePacket(Array(6) { MotorState.Speed })
-//    val bytes1 = MotorStatePacket.toByteArray(data1)
-//    val bytes2 = MotorStatePacket.toByteArray(data2)
-//    val bytes3 = MotorStatePacket.toByteArray(data3)
-    val data1 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
-    val data2 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
-    val data3 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
-    val bytes1 = MotorSpeedPacket.toByteArray(data1)
-    val bytes2 = MotorSpeedPacket.toByteArray(data2)
-    val bytes3 = MotorSpeedPacket.toByteArray(data3)
+    val data1 = MotorStatePacket(Array(6) { MotorState.Break })
+    val data2 = MotorStatePacket(Array(6) { MotorState.Stop })
+    val data3 = MotorStatePacket(Array(6) { MotorState.Speed })
+    val bytes1 = MotorStatePacket.toByteArray(data1)
+    val bytes2 = MotorStatePacket.toByteArray(data2)
+    val bytes3 = MotorStatePacket.toByteArray(data3)
+    val data4 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
+    val data5 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
+    val data6 = MotorSpeedPacket(FloatArray(6) { Random.nextDouble(-1.0, 1.0).toFloat() })
+    val bytes4 = MotorSpeedPacket.toByteArray(data4)
+    val bytes5 = MotorSpeedPacket.toByteArray(data5)
+    val bytes6 = MotorSpeedPacket.toByteArray(data6)
     SerialManager.startup()
     var i = 0
     Runtime.getRuntime().addShutdownHook(thread(false) {
         globalLogger.info {
-            "Sent $i."
+            "Sent $i packet(s)."
         }
     })
     while (true) {
-        val bytes = when (++i % 3) {
-            0    -> bytes3
+        val bytes = when (++i % 6) {
+            0    -> bytes6
             1    -> bytes1
             2    -> bytes2
+            3    -> bytes3
+            4    -> bytes4
+            5    -> bytes5
             else -> throw RuntimeException()
         }
         globalLogger.info {
-            SerialManager.send(bytes)
+            globalLogger.info("Send result ${SerialManager.send(bytes)} byte(s).")
             bytes.map { it.toInt() and 0xff }.joinToString()
         }
         Thread.sleep(args.first().toLong())
